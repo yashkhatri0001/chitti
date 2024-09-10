7764fc20083f0a93a2a0405e49a06e4bc59bef79
akhil2k-lc <150649110+akhil2k-lc@users.noreply.github.com>
2024-09-10 22:51:59 +0530

    User.jsx

--- Frontend/src/home/Leftpart/User.jsx
+++ Frontend/src/home/Leftpart/User.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import useConversation from "../../zustand/useConversation.js";
 import { useSocketContext } from "../../context/SocketContext.jsx";
 
+import profile from "../../../public/user.jpg";
+
 function User({ user }) {
   const { selectedConversation, setSelectedConversation } = useConversation();
   const isSelected = selectedConversation?._id === user._id;
@@ -17,7 +19,7 @@ function User({ user }) {
       <div className="flex space-x-4 px-8 py-3 hover:bg-slate-700 duration-300 cursor-pointer">
         <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
-            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img src={profile} />
           </div>
         </div>
         <div>
